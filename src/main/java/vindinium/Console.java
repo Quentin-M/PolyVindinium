c52dd4b58549bf8e5615dd224da69a1752934097
Quentin-M <me@quentin-machu.fr>
2014-12-13 20:31:45 +0100

    Updated launcher to add the number of games we shall play the and an optional server URL

--- src/main/java/vindinium/Console.java
+++ src/main/java/vindinium/Console.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import vindinium.bot.core.IBot;
+import vindinium.bot.core.alphabot.AbdadaBot;
 import vindinium.bot.core.alphabot.AlphaBot;
 import vindinium.bot.core.alphabot.FloydHeuristic;
 import vindinium.client.Config;
@@ -29,33 +30,51 @@ public class Console {
 	// http://vindinium.org/ai/rz3j6rr6
 	
 	public static void main(String[] args) throws InvalidConfigurationException {		
-		String secret;
-		GameMode mode;
-		int threads;
-		int depth;
+		String secret = "t6nnmjlf";
+		GameMode mode = GameMode.ARENA;
+		int gamesToPlay = 0;
+		int threads = 1;
+		int depth = 5;
 		
-		if(args.length == 4) {
+		String serverURL = null;
+		
+		if(args.length >= 5) {
 			secret = args[0];
-			if(args[1].equals("ARENA")) mode = GameMode.ARENA;
-			else mode = GameMode.TRAINING;
-			threads = Integer.parseInt(args[2]);
-			depth = Integer.parseInt(args[3]);
-		} else {
-			logger.info("Usage: [Secret] [ARENA|TRAINING] [Threads] [Depth]");
-			logger.info("Launching with default values");
 			
-			secret = "t6nnmjlf";
-			mode = GameMode.ARENA;
-			threads = 4;
-			depth = 5;
+			if(args[1].equals("ARENA")) mode = GameMode.ARENA;
+			else if(args[1].equals("TRAINING")) mode = GameMode.TRAINING;
+			else { printUsage(); System.exit(1); }
+			
+			try {
+				gamesToPlay = Integer.parseInt(args[2]);
+				threads = Integer.parseInt(args[3]);
+				depth = Integer.parseInt(args[4]);
+			} catch(Exception e) {
+				printUsage();
+				System.exit(1);
+			}
+			
+			if(args.length==6) serverURL = args[5];
+			
+			if(depth<1 || threads<1 || gamesToPlay<0) {
+				logger.error("Depth & thread values must be 1+. The number of games to play must be 0+.");
+				System.exit(1);
+			}
+		} else {
+			printUsage();
+			logger.info("Launching with default values");
 		}
 				
-		while(true) {
-			IBot bot = new AlphaBot(new FloydHeuristic(), depth);
+		int gamesPlayed = 0;
+		while(gamesToPlay == 0 || gamesPlayed < gamesToPlay) {
+			IBot bot;
+			if(threads==1) bot = new AlphaBot(new FloydHeuristic(), depth);
+			else bot = new AbdadaBot(new FloydHeuristic(), depth, threads);
 			
 			Config config = new Config();
 			config.setKey(secret);
 			config.setGameMode(mode);
+			if(serverURL!=null) config.setBaseUrl(serverURL);
 			config.validateConfiguration();
 			
 			Vindinium vindinium = new Vindinium(config);
@@ -75,6 +94,12 @@ public class Console {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
+			gamesPlayed++;
 		}
+	}
+	
+	private static void printUsage() {
+		logger.info("Usage: [Secret] [ARENA|TRAINING] [Number of Games (0 <=> No Limit)] [Threads] [Depth] {Server URL}");
 	}
 }
